f9e20d22c2a88fcecc458196378168dd0343377f
DanielArF <daniel_af96@hotmail.com>
2015-09-07 16:47:10 +0200

    Battleships_demo

---
+++ lib/grid.rb
@@ -0,0 +1,17 @@
+class Grid
+
+	attr_accessor :grid
+
+	def initialize
+		self.grid = { } 
+
+	#{:A1=>"water",:A2=>'water', :J10=>'water'}
+
+	[*"A".."B"].each do |l|
+		[*1..2].each do |n|
+
+
+		#@grid["#{l}#{n}".to.sym]="water"
+
+		end 
+end
---
+++ spec/grid_spec.rb
@@ -0,0 +1,17 @@
+require 'grid'
+
+descirbe Grid do
+	it 'can have a Ship'
+end
+
+describe size do
+	it 'size is two by two, A to B, 1 to 2'
+end 
+
+describe hit do
+	it 'can recivie a hit on a Ship'
+end 
+
+describe miss do
+	it 'can report a miss'
+end
